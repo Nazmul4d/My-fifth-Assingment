d37f35828cfe09db1bd624e8394e5aeef8fb17b3
Nazmul4d <smnazmulhasan456@gmail.com>
2024-12-10 01:42:20 +0600

    full project added by github repo

--- js/index.js
+++ js/index.js
@@ -1,72 +1,4 @@
-// document.getElementById('donate-btn')
-//           .addEventListener('click', function () {
-
-
-//           const noakhaliInput = document.getElementById('input-noakhali').value;
-//           const amntNoakhali = document.getElementById('noakhali-amnt').innerText;
-
-
-
-//           const inputNbr = parseFloat(noakhaliInput);
-//           const noakhaliNbr= parseFloat(amntNoakhali);
-
-
-
-
-//           const total = inputNbr + noakhaliNbr
-
-//           document.getElementById('noakhali-amnt').innerText = (total);
-
-
-
-//           const totalAmnt = document.getElementById('total-amount').innerText;
-//           const totalAmntNbr = parseFloat(totalAmnt);
-
-//           const totalBalance = totalAmntNbr - total ; 
-
-
-
-//           document.getElementById('total-amount').innerText = (totalBalance);
-
-
-
-//           })
-
-
-// document.getElementById('home-btn')
-//   .addEventListener('click', function () {
-
-//     window.location.href = '/home.html';
-//   })
-
-  // document.getElementById('blog-btn')
-  // .addEventListener('click', function () {
-  //   console.log('amahgd')
-  //   // window.location.href = '/index.html';
-  // });
-
-
-
-
-
 // NOAKHALI DONATION JS.CODE
-
-
-// const totalAmount = getTextFileValueById('total-amount');
-// const totalBalance = totalAmount - totalValue;
-
-// document.getElementById('total-amount').innerText = (totalBalance.toFixed(2));
-
-
-
-
-//  const noakhaliInput = getInputFileValueById('input-noakhali');
-//     const amntNoakhali = getTextFileValueById('noakhali-amnt');
-//     const totalAmount = getTextFileValueById('total-amount');
-
-
-
-
 
 document.getElementById('donate-btn')
   .addEventListener('click', function () {
@@ -81,22 +13,43 @@ document.getElementById('donate-btn')
 
       document.getElementById('noakhali-amnt').innerText = (totalValue.toFixed(2));
 
-    
-      const totalBalance = totalAmount - totalValue;
+
+      const totalBalance = totalAmount - noakhaliInput;
 
       document.getElementById('total-amount').innerText = (totalBalance.toFixed(2));
+
      
-      
       document.getElementById('my_modal_5').showModal();
+
+
+      const date = new Date()
+      const historyForm = document.getElementById('btn-history-from')
+      const noakhaliTitle = document.getElementById('noakhali-title').innerText
+      const div = document.createElement('div');
+      div.innerHTML = `
+      
+      <div class="w-11/12 mx-auto border border-[#B4F461] rounded-sm m-4">
      
+      <div class="p-5 space-y-4">
+                  <h3 class ="font-bold ">${noakhaliInput} Taka ${noakhaliTitle}</h3>
+      <p class="text-[10px]"> ${date}</p>
+      </div>
+      
+      </div>
+      
+      
+      `
+      historyForm.appendChild(div);
+
     }
 
     else {
       alert("Please Provide A Number")
     }
 
-    document.getElementById('input-noakhali').value=" ";
-    
+    const value = document.getElementById('input-noakhali').value = " ";
+    console.log(value)
+
   })
 
 // FENI DONATION JS.CODE
@@ -110,17 +63,41 @@ document.getElementById('donate-btn2')
 
 
 
-    if (!isNaN(feniInput) && feniInput >= 0 ) {
+    if (!isNaN(feniInput) && feniInput >= 0) {
       const totalValue2 = feniInput + amntFeni;
 
       document.getElementById('Feni-amount').innerText = (totalValue2.toFixed(2));
 
-      // const totalAmount2 = getTextFileValueById('total-amount');
-      const totalBalance = totalAmount- totalValue2;
+      const totalBalance = totalAmount - feniInput;
 
       document.getElementById('total-amount').innerText = (totalBalance.toFixed(2));
-       
+
       document.getElementById('my_modal_5').showModal();
+
+
+      const date = new Date()
+      const historyForm = document.getElementById('btn-history-from')
+      const feniTitle = document.getElementById('feni-title').innerText;
+      const div = document.createElement('div')
+      div.innerHTML = `
+          
+
+      <div class="w-11/12 mx-auto border border-[#B4F461] rounded-sm m-4">
+     
+      <div class="p-5 space-y-4">
+                  <h3 class ="font-bold ">${feniInput} Taka ${feniTitle}</h3>
+      <p class="text-[10px]"> ${date}</p>
+      </div>
+      
+      </div>
+      
+      
+      `
+
+      historyForm.appendChild(div)
+
+
+
     }
 
     else {
@@ -149,20 +126,41 @@ document.getElementById('donate-btn3')
 
       document.getElementById('quota-amount').innerText = (totalValue3.toFixed(2));
 
-      // const totalAmount3 = getTextFileValueById('total-amount');
-      const totalBalance = totalAmount - totalValue3;
+      const totalBalance = totalAmount - quotaInput;
 
       document.getElementById('total-amount').innerText = (totalBalance.toFixed(2));
       document.getElementById('my_modal_5').showModal();
 
+      const date = new Date()
+      const historyForm = document.getElementById('btn-history-from')
+      const qutaTitle = document.getElementById('quota-title').innerText;
+      const div = document.createElement('div')
+      div.innerHTML = `
+          
+
+      <div class="w-11/12 mx-auto border border-[#B4F461] rounded-sm m-4">
+     
+      <div class="p-5 space-y-4">
+                  <h3 class ="font-bold ">${quotaInput} Taka ${qutaTitle}</h3>
+      <p class="text-[10px]"> ${date}</p>
+      </div>
+      
+      </div>
+      
+      
+      `
+
+      historyForm.appendChild(div)
     }
+
 
     else {
       alert("Please Provide A Number")
     }
 
-    document.getElementById('input-quota').value=" ";
-    
+    document.getElementById('input-quota').value = " ";
+
+
   })
 
 
@@ -184,7 +182,7 @@ document.getElementById('btn-history')
 
     document.getElementById('btn-history').classList.add('common-clr-btn');
     document.getElementById('btn-donate').classList.remove('common-clr-btn');
-    
+
 
 
   })
@@ -193,12 +191,7 @@ document.getElementById('btn-donate')
 
     document.getElementById('btn-donate').classList.add('common-clr-btn');
     document.getElementById('btn-history').classList.remove('common-clr-btn');
-  
+
 
   })
 
-  // historyItems
-
-  document.getElementById('history-list').addEventListener('click',function(){
-   
-  })
